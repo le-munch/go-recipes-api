f31471a75322561bea3c4c1d4b0baeb994a55f30
letonin <letonin@letonin.de>
2026-01-06 16:55:33 +0100

    more ingredient data

--- go-recipes-api/db-seed.sql
+++ go-recipes-api/db-seed.sql
@@ -6,15 +6,17 @@ CREATE TABLE Recipes
     Description     NVARCHAR(MAX),
     CreationDate    DATETIME DEFAULT GETDATE(),
     PreparationTime INT, -- stored in minutes
-    Rating          DECIMAL(2,1) NULL
+    Rating          TINYINT NULL
 );
 
--- Create table for ingredients
+-- Create table for ingredients. Each ingredient is linked to a recipe. The recipe is default for 2 portions
 CREATE TABLE RecipeIngredients
 (
     IngredientId INT IDENTITY (1,1) PRIMARY KEY,
     RecipeId     INT FOREIGN KEY REFERENCES Recipes (RecipeId),
-    Ingredient   NVARCHAR(255) NOT NULL
+    Ingredient   NVARCHAR(255) NOT NULL,
+    Amount INT NOT NULL,
+    Metric NVARCHAR(50) NULL
 );
 
 -- Create table for steps
@@ -23,11 +25,11 @@ CREATE TABLE RecipeSteps
     StepId          INT IDENTITY (1,1) PRIMARY KEY,
     RecipeId        INT FOREIGN KEY REFERENCES Recipes (RecipeId),
     StepNumber      INT           NOT NULL,
-    StepDescription NVARCHAR(MAX) NOT NULL
+    StepDescription NVARCHAR(MAX) NOT NULL,
 );
 
 -- Create indexes for better performance
 CREATE INDEX IX_RecipeIngredients_RecipeId ON RecipeIngredients (RecipeId);
 CREATE INDEX IX_RecipeSteps_RecipeId ON RecipeSteps (RecipeId);
 
-ALTER TABLE Recipes ADD CONSTRAINT CK_Recipes_Rating CHECK (Rating BETWEEN 1.0 AND 5.0);
+ALTER TABLE Recipes ADD CONSTRAINT CK_Recipes_Rating_1_10 CHECK (Rating BETWEEN 1 AND 10);
